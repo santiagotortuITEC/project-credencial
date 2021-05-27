38fe68dbc2615e123082f7b06d74fe05f7a8019e
santiagotortuITEC <s.tortu@itecriocuarto.org.ar>
2021-05-27 13:13:11 -0300

    last version

--- config/url_api.js
+++ config/url_api.js
@@ -1,3 +1,3 @@
-//export const url = 'http://64.225.47.18:8080/';
-export const url = 'http://192.168.0.7:3001/';
+export const url = 'http://64.225.47.18:8080/';
+//export const url = 'http://192.168.0.8:3001/';
  
